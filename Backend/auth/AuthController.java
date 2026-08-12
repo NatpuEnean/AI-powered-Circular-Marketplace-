00c165bb8d5885865ece4c80ed42da0947aaf587
NatpuEnean <natpuenean@gmail.com>
2026-08-12 14:21:49 +0530

    backend

---
+++ Backend/auth/AuthController.java
@@ -0,0 +1,39 @@
+package com.example.backend.controller;
+
+import com.example.backend.dto.AuthRequest;
+import com.example.backend.dto.AuthResponse;
+import com.example.backend.dto.RegisterRequest;
+import com.example.backend.service.AuthService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(
+            @RequestBody RegisterRequest request) {
+
+        return ResponseEntity.ok(
+                authService.register(request)
+        );
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(
+            @RequestBody AuthRequest request) {
+
+        return ResponseEntity.ok(
+                authService.login(request)
+        );
+    }
+}
